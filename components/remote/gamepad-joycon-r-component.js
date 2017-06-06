08cd0501c05218112cba5ecdbba1820eccc2b917
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-06-06 22:43:46 +0200

    --wip--

---
+++ components/remote/gamepad-joycon-r-component.js
@@ -0,0 +1,146 @@
+(function () {
+
+  'use strict'
+
+  // ADD a prefix in the name
+  const componentsChannel = new BroadcastChannel(`COMPONENTS_CHANNEL(default)`)
+
+  const gamepad = new Gamepad()
+
+  gamepad.setCustomMapping('gamepad', {
+    'start': 9,
+    'select': 12,
+    'shoulder_top_right': 14,
+    'shoulder_top_left': 11,
+    'shoulder_bottom_right': 15,
+    'd_pad_left': 4,
+    'd_pad_right': 5,
+    'button_1': 0,
+    'button_2': 1,
+    'button_3': 2,
+    'button_4': 3,
+  })
+
+  gamepad.setCustomMapping('keyboard', {})
+
+  gamepad.on('press', 'shoulder_top_left', () => {
+    componentsChannel.postMessage({
+      command: 'go-to-previous-step',
+      commandArgs: { secret: false },
+    })
+  })
+
+  gamepad.on('press', 'shoulder_top_right', () => {
+    componentsChannel.postMessage({
+      command: 'go-to-next-step',
+      commandArgs: { secret: false },
+    })
+  })
+
+  gamepad.on('press', 'shoulder_bottom_right', () => {
+    componentsChannel.postMessage({
+      command: 'go-to-next-step',
+      commandArgs: { secret: false },
+    })
+  })
+
+  gamepad.on('press', 'd_pad_left', () => {
+    componentsChannel.postMessage({
+      command: 'go-to-previous-step',
+      commandArgs: { secret: true },
+    })
+  })
+
+  gamepad.on('press', 'd_pad_right', () => {
+    componentsChannel.postMessage({
+      command: 'go-to-next-step',
+      commandArgs: { secret: true },
+    })
+  })
+
+  gamepad.on('press', 'd_pad_up', () => {
+    componentsChannel.postMessage({
+      command: 'move-notes',
+      commandArgs: { amount: -90 },
+    })
+  })
+
+  gamepad.on('press', 'd_pad_down', () => {
+    componentsChannel.postMessage({
+      command: 'move-notes',
+      commandArgs: { amount: 90 },
+    })
+  })
+
+  gamepad.on('hold', 'button_3', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'one', enabled: true },
+    })
+  })
+  gamepad.on('release', 'button_3', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'one', enabled: false },
+    })
+  })
+  gamepad.on('hold', 'button_4', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'two', enabled: true },
+    })
+  })
+  gamepad.on('release', 'button_4', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'two', enabled: false },
+    })
+  })
+  gamepad.on('hold', 'button_1', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'three', enabled: true },
+    })
+  })
+  gamepad.on('release', 'button_1', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'three', enabled: false },
+    })
+  })
+  gamepad.on('hold', 'button_2', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'four', enabled: true },
+    })
+  })
+  gamepad.on('release', 'button_2', () => {
+    componentsChannel.postMessage({
+      command: 'toggle-slide-deck-state',
+      commandArgs: { state: 'four', enabled: false },
+    })
+  })
+
+  gamepad.on('press', 'select', () => {
+    console.log('select')
+    componentsChannel.postMessage({
+      command: 'reset-stopwatch',
+      commandArgs: {},
+    })
+    componentsChannel.postMessage({
+      command: 'reset-timer',
+      commandArgs: {},
+    })
+  })
+  gamepad.on('press', 'start', () => {
+    console.log('start')
+    componentsChannel.postMessage({
+      command: 'toggle-stopwatch',
+      commandArgs: {},
+    })
+    componentsChannel.postMessage({
+      command: 'toggle-timer',
+      commandArgs: {},
+    })
+  })
+})()
