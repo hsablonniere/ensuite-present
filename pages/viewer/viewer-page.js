d61f4b05299debe247ab02ac46c1b0ca094df5a3
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-04-12 12:12:03 +0200

    --wip--

--- pages/viewer/viewer-page.js
+++ pages/viewer/viewer-page.js
@@ -1,58 +1,67 @@
 (function () {
 
-  'use strict'
+  'use strict';
 
-  const params = new URLSearchParams(window.location.search)
+  const params = new URLSearchParams(window.location.search);
 
   // open a slide deck
   // default : slide-deck-url=about:blank
-  const slideDeckUrl = params.get('slide-deck-url') || 'about:blank'
-  const initCursor = params.get('init-cursor') || null
+  const slideDeckUrl = params.get('slide-deck-url') || 'about:blank';
+  const initCursor = params.get('init-cursor') || null;
 
   // display a grid of slide deck
   // ex: multi=3 => grid of 9 slide decks (3 by 3)
   // default : multi=1
-  const multi = Number(params.get('multi')) || 1
+  const multi = Number(params.get('multi')) || 1;
 
   // display the slide deck in advance or with delay
   // shift=1 will display the very next step
   // default : shift=0
-  const shift = Number(params.get('shift')) || 0
+  const shift = Number(params.get('shift')) || 0;
 
   // connect to a named event bus
   // this allows to have several groups of slide decks that are not synced together
   // default : bus=default
-  const bus = params.get('bus') || 'default'
+  const bus = params.get('bus') || 'default';
 
   // apply "secret" goTo() only if secret is enabled
   // this allows a speaker to move slides on his screen and not on the projector
   // default : secret=false
-  const secret = (params.get('secret') === 'true') ? true : false
+  const secret = (params.get('secret') === 'true') ? true : false;
 
-  const wrapperReference = document.querySelector('.slideViewer-wrapper')
+  const wrapperReference = document.querySelector('.slideViewer-wrapper');
 
-  wrapperReference.dataset.secret = secret
-  wrapperReference.dataset.bus = bus
+  wrapperReference.dataset.secret = secret;
+  wrapperReference.dataset.bus = bus;
 
   if (multi === 1) {
-    wrapperReference.dataset.shift = shift
+    wrapperReference.dataset.shift = shift;
   }
   else {
-    document.body.classList.add('multi')
-    document.body.setAttribute('style', `--multi: ${multi}`)
+    document.body.classList.add('multi');
+    document.body.setAttribute('style', `--multi: ${multi}`);
 
-    const before = wrapperReference.nextSibling
+    const before = wrapperReference.nextSibling;
 
     for (let i = 1; i < multi * multi; i += 1) {
-      const newWrapper = wrapperReference.cloneNode(true)
-      newWrapper.dataset.shift = i + shift
-      wrapperReference.parentElement.insertBefore(newWrapper, before)
+      const newWrapper = wrapperReference.cloneNode(true);
+      newWrapper.dataset.shift = i + shift;
+      wrapperReference.parentElement.insertBefore(newWrapper, before);
     }
 
-    wrapperReference.dataset.shift = 0 + shift
+    wrapperReference.dataset.shift = 0 + shift;
   }
 
-  const componentsChannel = new BroadcastChannel(`COMPONENTS_CHANNEL(${bus})`)
-  componentsChannel.postMessage({ command: 'load-slide-deck', commandArgs: { url: slideDeckUrl, initCursor } })
-  console.log(`loading ${slideDeckUrl}, ${initCursor}`)
-})()
+  let hideCursorTimeoutId;
+  window.addEventListener('mousemove', (e) => {
+    clearTimeout(hideCursorTimeoutId);
+    wrapperReference.classList.add('with-cursor');
+    hideCursorTimeoutId = setTimeout(() => {
+      wrapperReference.classList.remove('with-cursor');
+    }, 5000);
+  });
+
+  const componentsChannel = new BroadcastChannel(`COMPONENTS_CHANNEL(${bus})`);
+  componentsChannel.postMessage({ command: 'load-slide-deck', commandArgs: { url: slideDeckUrl, initCursor } });
+  console.log(`loading ${slideDeckUrl}, ${initCursor}`);
+})();
